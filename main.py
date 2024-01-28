9106ec2e3145a011c7d8aa01eb5eef52cf6dce3a
FeDuck113 <feo13042007dor@gmail.com>
2024-01-28 20:29:03 +0300

    add F_test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import config
 import sys
 
 def calculate_variance(data):
@@ -5,38 +6,61 @@ def calculate_variance(data):
     n_param = len(data[0]) - 1 #мэйби -1, чтоб не учитывать результат
 
     #рассчёт средних значений для ка
-    mean = list()
+    means = list()
     for i in range(n_param):
-        mean.append(sum(exp[i] for exp in data)/n_exp)
+        means.append(sum(exp[i] for exp in data)/n_exp)
 
     #расчёт дисперсии для каждого значения параметра
     variances = list()
     for i in range(n_exp):
         variances_exp = list()
         for j in range(n_param):
-            variances_exp.append((data[i][j] - mean[j]) ** 2 / (n_exp - 1))
+            variances_exp.append((data[i][j] - means[j]) ** 2 / (n_exp - 1))
         variances.append(list(variances_exp))
 
-    return variances
+    return variances, means
 
 
-def cochrans_test(variances):
-    # G = max(variances)/sum(s for s in variances)
+def cochrans_test(variances):    #добавить рассчёт дисперсии адекватности
+    n_exp = len(variances)
+
     max_variances = max(max(var) for var in variances)
     sum_variances = sum(sum(var) for var in variances)
-    G = max_variances / sum_variances
-    return G
 
+    G = max_variances / sum_variances       #Коэффициент Кохрена
+    exp_variances = sum_variances/n_exp     #дисперсия экспиримента
+
+    return G, exp_variances
+
+def F_test(data, means, exp_variance):
+    n_exp = len(data)
+    n_param = len(data[0])
+
+    f = n_exp * n_param - n_exp     #число степеней свободы
+
+    # расчёт дисперсии адекватности
+    adeq_variences = 0
+    for j in range(n_param):
+        adeq_variences += (sum((means[j]-data[i][j])**2 / f for i in range(n_exp)))
+
+    #расчёт критерия Фишера
+    F = adeq_variences / exp_variance
+
+    return F
 
 
 a = [[3, 9, 7, 67], [1, 7, 5, 12], [10, 17, 25, 800]]
 
 
 
-variances = calculate_variance(a)
-G = cochrans_test(variances)
-if G > 0.6798:
+variances, means = calculate_variance(a)
+G, exp_variance = cochrans_test(variances)
+print(G, exp_variance)
+
+if G > config.G_STANDART:  #если больше табличного значения, то ряд дисперсий неоднородный
     sys.exit()
+
+F_test(a, means, exp_variance)
 
 
 
