b824e3a57dd16256efafa2d7a66bcf960057566b
FeDuck113 <feo13042007dor@gmail.com>
2025-05-27 19:37:41 +0300

    add style to web

--- webInterface.py
+++ webInterface.py
@@ -1,11 +1,13 @@
 import uvicorn
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import main
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
